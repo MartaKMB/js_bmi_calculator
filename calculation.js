96cd51c04ef51ea63cc4a2e2df50e18f0a37666f
MartaKMB <muszkamarta@interia.pl>
2017-07-26 16:32:52 +0200

    Added styling in js

--- calculation.js
+++ calculation.js
@@ -3,13 +3,15 @@
 var label_weight = document.createElement('label');
 var node_weight = document.createTextNode('Podaj swoją wagę w kilogramach');
 label_weight.appendChild(node_weight);
+label_weight.id = "id_label_weight"
 
 var input_weight = document.createElement('input');
 input_weight.id = 'id_weight';
 		
 var label_height = document.createElement('label');
 var node_height = document.createTextNode('Podaj wzrost w metrach');
-label_height.appendChild(node_height);		
+label_height.appendChild(node_height);	
+label_height.id = 'id_label_height'	
 		
 var input_height = document.createElement('input');
 input_height.id = 'id_height';
@@ -85,21 +87,59 @@ function messageBMI(numA, numB) {
 	if (result_to_message < 18.5) {
 		
 		document.getElementById('result_div_message').innerHTML = 'Niedowaga';
+		document.getElementById('result_div_message').style.color = '#a17944';	
 				
 	} else if (result_to_message < 24.9) {
 		
 		document.getElementById('result_div_message').innerHTML = 'Waga w normie';
+		document.getElementById('result_div_message').style.color = '#49a109';
+		
 				
 	} else if (result_to_message < 29.9) {
 		
 		document.getElementById('result_div_message').innerHTML = 'Nadwaga';
+		document.getElementById('result_div_message').style.color = '#a17944';
 				
 	} else if (result_to_message > 30) {
 		
 		document.getElementById('result_div_message').innerHTML = 'Otyłość';
+		document.getElementById('result_div_message').style.color = '#dd2d00';
+		
+		
 			
 	} else {
 				
 		document.getElementById('result_div_message').innerHTML = 'Spróbuj jeszcze raz!';
+		document.getElementById('result_div_message').style.color = '#aaaaaa';
 	}
 }
+
+/* styling */
+
+document.getElementById('id_label_height').style.textAlign = 'center';
+document.getElementById('id_label_weight').style.textAlign = 'center';
+
+document.getElementById('id_height').style.display = 'block';
+document.getElementById('id_height').style.margin = '10px auto 10px auto';
+document.getElementById('id_height').style.border = '2px solid #666666';
+document.getElementById('id_height').style.backgroundColor = '#aaaaaa';
+document.getElementById('id_height').style.height = '26px';
+
+document.getElementById('id_weight').style.display = 'block';
+document.getElementById('id_weight').style.margin = '10px auto 10px auto';
+document.getElementById('id_weight').style.border = '2px solid #666666';
+document.getElementById('id_weight').style.backgroundColor = '#aaaaaa';
+document.getElementById('id_weight').style.height = '26px';
+
+document.getElementById('id_button').style.display = 'block';
+document.getElementById('id_button').style.margin = '10px auto 10px auto';
+document.getElementById('id_button').style.padding = '10px';
+document.getElementById('id_button').style.backgroundColor = '#3eacff';
+document.getElementById('id_button').style.border = '2px solid #3eacff';
+document.getElementById('id_button').style.color = '#ffffff';
+document.getElementById('id_button').style.height = '26px';
+
+document.getElementById('result_div_number').style.textAlign = 'center';
+
+document.getElementById('result_div_message').style.textAlign = 'center';
+
